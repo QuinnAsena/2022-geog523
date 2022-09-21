d50f648aed42f991cfa3a045cbfb9fb63f8a4415
QuinnAsena <qase352@aucklanduni.ac.nz>
2022-09-21 18:03:39 -0500

    Major updates.

--- 02-intro-to-R/Lab2_Demo.R
+++
@@ -1,8 +0,0 @@
-##---------------------------------------
-## Author:  [YOUR NAME]
-## Date:  [DATE]
-## Lab 2:  Introduction to R and RStudio
-##---------------------------------------
-
-## Section 1.3: demonstration of concatenate operator: c()
-dieroll <- c(2,5,1,6,5,5,4,1)
